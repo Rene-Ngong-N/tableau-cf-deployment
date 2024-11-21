5ce32332f106c45224d8befc598a18625baf682b
Rene-Ngong <renengong16@gmail.com>
2024-11-21 11:52:34 -0600

    added parameter for cf

---
+++ nestedresource.js
@@ -0,0 +1,50 @@
+[
+    {
+        "ParameterKey": "TableauServerSubdomain",
+        "ParameterValue": "tableau"
+    },
+    {
+        "ParameterKey": "Route53HostedZone",
+        "ParameterValue": "Z0322061AZI6Q2VHWYLH"
+    },
+    {
+        "ParameterKey": "QSS3BucketName",
+        "ParameterValue": "aws-ia"
+    },
+    {
+        "ParameterKey": "PublicSubnet1ID",
+        "ParameterValue": "subnet-092f4c86e7184cbe1"
+    },
+    {
+        "ParameterKey": "Route53DomainName",
+        "ParameterValue": "demo-tableau.com"
+    },
+    {
+        "ParameterKey": "VPCID",
+        "ParameterValue": "vpc-080afb2ebc3ae29c2"
+    },
+    {
+        "ParameterKey": "SourceCIDR",
+        "ParameterValue": "0.0.0.0/0"
+    },
+    {
+        "ParameterKey": "PublicSubnet2ID",
+        "ParameterValue": "subnet-02a923916a63e6a02"
+    },
+    {
+        "ParameterKey": "QSS3BucketRegion",
+        "ParameterValue": "us-east-1"
+    },
+    {
+        "ParameterKey": "QSS3KeyPrefix",
+        "ParameterValue": "cfn-ps-tableau-server/"
+    },
+    {
+        "ParameterKey": "TsmSubdomain",
+        "ParameterValue": "tableau-tsm"
+    },
+    {
+        "ParameterKey": "TableauS3BucketName",
+        "ParameterValue": "tableau-demo-bmf"
+    }
+]
---
+++ parentparams.js
@@ -0,0 +1,189 @@
+[
+    {
+        "ParameterKey": "TableauServerSubdomain",
+        "ParameterValue": "tableau"
+    },
+    {
+        "ParameterKey": "Route53HostedZone",
+        "ParameterValue": "Z0322061AZI6Q2VHWYLH"
+    },
+    {
+        "ParameterKey": "QSS3BucketName",
+        "ParameterValue": "aws-ia"
+    },
+    {
+        "ParameterKey": "PublicSubnet1ID",
+        "ParameterValue": "subnet-092f4c86e7184cbe1"
+    },
+    {
+        "ParameterKey": "Route53DomainName",
+        "ParameterValue": "demo-tableau.com"
+    },
+    {
+        "ParameterKey": "VPCID",
+        "ParameterValue": "vpc-080afb2ebc3ae29c2"
+    },
+    {
+        "ParameterKey": "SourceCIDR",
+        "ParameterValue": "0.0.0.0/0"
+    },
+    {
+        "ParameterKey": "PublicSubnet2ID",
+        "ParameterValue": "subnet-02a923916a63e6a02"
+    },
+    {
+        "ParameterKey": "QSS3BucketRegion",
+        "ParameterValue": "us-east-1"
+    },
+    {
+        "ParameterKey": "QSS3KeyPrefix",
+        "ParameterValue": "cfn-ps-tableau-server/"
+    },
+    {
+        "ParameterKey": "TsmSubdomain",
+        "ParameterValue": "tableau-tsm"
+    },
+    {
+        "ParameterKey": "TableauS3BucketName",
+        "ParameterValue": "tableau-demo-bmf"
+[
+    {
+        "ParameterKey": "RegIndustry",
+        "ParameterValue": "Technology"
+    },
+    {
+        "ParameterKey": "TableauServerAdminPassword",
+        "ParameterValue": "****"    },
+    {
+        "ParameterKey": "VPCID",
+        "ParameterValue": "vpc-080afb2ebc3ae29c2"
+    },
+    {
+        "ParameterKey": "TsmUsername",
+        "ParameterValue": "tsmadmin"
+    },
+    {
+        "ParameterKey": "QSS3KeyPrefix",
+        "ParameterValue": "cfn-ps-tableau-server/"
+    },
+    {
+        "ParameterKey": "RegLastName",
+        "ParameterValue": "Tableau"
+    },
+    {
+        "ParameterKey": "RegPhone",
+        "ParameterValue": "7207710071"
+    },
+    {
+        "ParameterKey": "RegFirstName",
+        "ParameterValue": "Jim"
+    },
+    {
+        "ParameterKey": "RegTitle",
+        "ParameterValue": "Owner"
+    },
+    {
+        "ParameterKey": "RegDepartment",
+        "ParameterValue": "Infra"
+    },
+    {
+        "ParameterKey": "RegCompany",
+        "ParameterValue": "IBM"
+    },
+    {
+        "ParameterKey": "InstanceType",
+        "ParameterValue": "m5.8xlarge"
+    },
+    {
+        "ParameterKey": "RegCity",
+        "ParameterValue": "Denver"
+    },
+    {
+        "ParameterKey": "TableauS3BucketName",
+        "ParameterValue": "tableau-demo-bmf"
+    },
+    {
+        "ParameterKey": "RegCompanyEmployees",
+        "ParameterValue": "1"
+    },
+    {
+        "ParameterKey": "KeyPairName",
+        "ParameterValue": "defaultvpc-ec2"
+    },
+    {
+        "ParameterKey": "QSS3BucketName",
+        "ParameterValue": "aws-ia"
+    },
+    {
+        "ParameterKey": "RegZip",
+        "ParameterValue": "80229"
+    },
+    {
+        "ParameterKey": "RegOptIn",
+        "ParameterValue": ""
+    },
+    {
+        "ParameterKey": "OS",
+        "ParameterValue": "Linux"
+    },
+    {
+        "ParameterKey": "AcceptEULA",
+        "ParameterValue": ""
+    },
+    {
+        "ParameterKey": "SourceCIDR",
+        "ParameterValue": "0.0.0.0/0"
+    },
+    {
+        "ParameterKey": "TsmPassword",
+        "ParameterValue": "****"
+    },
+    {
+        "ParameterKey": "PrivateSubnet1ID",
+        "ParameterValue": "subnet-0431940404fc0dee6"
+    },
+    {
+        "ParameterKey": "RegCountry",
+        "ParameterValue": "United States"
+    },
+    {
+        "ParameterKey": "Route53HostedZone",
+        "ParameterValue": "Z0322061AZI6Q2VHWYLH"
+    },
+    {
+        "ParameterKey": "RegEmail",
+        "ParameterValue": "jcporter@us.ibm.com"
+    },
+    {
+        "ParameterKey": "Route53DomainName",
+        "ParameterValue": "demo-tableau.com"
+    },
+    {
+        "ParameterKey": "PublicSubnet1ID",
+        "ParameterValue": "subnet-092f4c86e7184cbe1"
+    },
+    {
+        "ParameterKey": "PublicSubnet2ID",
+        "ParameterValue": "subnet-02a923916a63e6a02"
+    },
+    {
+        "ParameterKey": "QSS3BucketRegion",
+        "ParameterValue": "us-east-1"
+    },
+    {
+        "ParameterKey": "TableauServerAdminUser",
+        "ParameterValue": "admin"
+    },
+    {
+        "ParameterKey": "RegState",
+        "ParameterValue": "CO"
+    },
+    {
+        "ParameterKey": "TableauServerSubDomainName",
+        "ParameterValue": "tableau"
+    },
+    {
+        "ParameterKey": "LicenseKey",
+        "ParameterValue": ""
+    }
+]
